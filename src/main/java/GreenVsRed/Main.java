eaac74f44fa8395242fe48bd62cc2ccb91dec71f
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-07-20 18:07:03 +0300

    added exceptions in class Main

--- src/main/java/GreenVsRed/Main.java
+++ src/main/java/GreenVsRed/Main.java
@@ -1,48 +1,86 @@
 package main.java.GreenVsRed;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        GreenVsRed simulate = null;
+        int xWidth = 0;
+        int yHeight = 0;
 
-
-        int[] dimension = Arrays.stream(scanner.nextLine().split(","))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        int xWidth = dimension[0];
-        int yHeight = dimension[1];
-
-        GreenVsRed simulate = new GreenVsRed(xWidth, yHeight);
-
-        for (int i = 0; i < xWidth; i++) {
-            int[] stringLine = Arrays.stream(scanner.nextLine().split(""))
+        try {
+            int[] dimension = Arrays.stream(scanner.nextLine().split(","))
                     .mapToInt(Integer::parseInt)
                     .toArray();
 
-            for (int j = 0; j < yHeight; j++) {
-                if (stringLine[i] == 1) {
-                    simulate.setGreen(i, j);
-                } else {
-                    simulate.setRed(i, j);
-                }
+            if (dimension.length != 2) {
+                System.out.println("Board must be initialized as 2D grid.");
+                throw new ArrayIndexOutOfBoundsException();
             }
+
+            xWidth = dimension[0];
+            yHeight = dimension[1];
+
+            simulate = new GreenVsRed(xWidth, yHeight);
+        } catch (InputMismatchException exception) {
+            System.out.println("Not an integer, please try again. Press enter key to exit");
+
+        } catch (NumberFormatException ex) {
+            System.out.println("Input must contain only numbers separated by coma.");
+            System.out.println("Exception is caused " + ex.getMessage());
         }
 
-        int[] coordNumbStr = Arrays.stream(scanner.nextLine().split(","))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        try {
+            for (int i = 0; i < xWidth; i++) {
+                int[] stringLine = Arrays.stream(scanner.nextLine().split(""))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
 
-        scanner.close();
+                for (int j = 0; j < yHeight; j++) {
+                    if (stringLine[i] < 0 || stringLine[i] > 1) {
+                        throw new IOException();
+                    }
 
-        simulate.wantedX = coordNumbStr[0];
-        simulate.wantedY = coordNumbStr[1];
-        int turns = coordNumbStr[2];
+                    if (stringLine[i] == 1) {
+                        simulate.setGreen(i, j);
+                    } else {
+                        simulate.setRed(i, j);
+                    }
+                }
+            }
+        } catch (InputMismatchException exception) {
+            System.out.println("Not an integer, please try again. Press enter key to exit");
+//
+        } catch (NumberFormatException ex) {
+            System.out.println("Input must contain only numbers.");
+            System.out.println("Exception is caused " + ex.getMessage());
+        } catch (IOException e) {
+            System.out.println("Cells can contain only numbers 1 or 0.");
+            e.printStackTrace();
+        }
 
-        simulate.turnExact(turns);
-        System.out.println(simulate.getBeenGreen());
+        try {
+            int[] coordNumbStr = Arrays.stream(scanner.nextLine().split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            assert simulate != null;
+            simulate.wantedX = coordNumbStr[0];
+            simulate.wantedY = coordNumbStr[1];
+            int turns = coordNumbStr[2];
+
+            simulate.turnExact(turns);
+            System.out.println(simulate.getBeenGreen());
+        } catch (InputMismatchException exception) {
+            System.out.println("Not an integer, please try again. Press enter key to exit");
+        } catch (NumberFormatException ex) {
+            System.out.println("Input must contain only numbers separated by coma.");
+            System.out.println("Exception is caused " + ex.getMessage());
+        }
 
     }
 }
